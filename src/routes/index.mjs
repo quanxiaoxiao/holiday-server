a233c7ef877b0afbf965d73717da9c7f14d7707d
quan <quan_xiaoxiao@qq.com>
2025-02-18 15:37:24 +0800

    xxx

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -3,6 +3,7 @@ import path from 'node:path';
 
 import { toDataify } from '@quanxiaoxiao/node-utils';
 
+import getWeeklyRangeOfDateTime from '../controllers/getWeeklyRangeOfDateTime.mjs';
 import { getState } from '../store/store.mjs';
 import compenstationDay from './compenstationDay/index.mjs';
 import holiday from './holiday/index.mjs';
@@ -13,6 +14,19 @@ export default {
       fs.writeFileSync(path.resolve(process.cwd(), '.state.json'), toDataify(getState()));
       ctx.response = {
         data: Date.now(),
+      };
+    },
+  },
+  '/api/weekly-range': {
+    query: {
+      dateTime: {
+        type: 'number',
+      },
+    },
+    get: async (ctx) => {
+      const list = await getWeeklyRangeOfDateTime(ctx.request.query.dateTime ?? Date.now());
+      ctx.response = {
+        data: list,
       };
     },
   },
