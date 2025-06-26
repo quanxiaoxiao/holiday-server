9cd7fb0d54b2c2c088b8a8eabd47756741fb9b6e
quan <quan_xiaoxiao@qq.com>
2025-06-26 14:47:38 +0800

    xxx

---
+++ src/models/dateMarker.mjs
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const dateMarkerSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  label: {
+    type: String,
+    maxLength: 1,
+    trim: true,
+  },
+  dateTimeCreate: {
+    type: Number,
+    default: Date.now,
+    index: true,
+  },
+  dateTimeUpdate: {
+    type: Number,
+    default: Date.now,
+  },
+  dateTime: {
+    type: Number,
+    required: true,
+    index: true,
+  },
+  color: {
+    type: String,
+  },
+  icon: {
+    type: String,
+  },
+  description: {
+    type: String,
+    trim: true,
+    default: '',
+  },
+  invalid: {
+    type: Boolean,
+    index: true,
+    default: false,
+  },
+  dateTimeInvalid: {
+    type: Number,
+  },
+});
+
+export default dateMarkerSchema;
--- src/models/index.mjs
+++ src/models/index.mjs
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 
 import absenceDaySchema from './absenceDay.mjs';
 import compenstationDaySchema from './compenstationDay.mjs';
+import dateMarkerSchema from './dateMarker.mjs';
 import holidaySchema from './holiday.mjs';
 
 export const Holiday = mongoose.model('Holiday', holidaySchema);
 export const AbsenceDay = mongoose.model('AbsenceDay', absenceDaySchema);
 export const CompenstationDay = mongoose.model('CompenstationDay', compenstationDaySchema);
+export const DateMarker = mongoose.model('DateMarker', dateMarkerSchema);
